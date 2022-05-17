f704bb3dcb6e6d6065accaf60b9e29e64da7e451
Raoji Patil <raojipatil22@gmail.com>
2022-05-17 17:24:24 +0530

    update

--- assets/php/instagram.php
+++ assets/php/instagram.php
@@ -1,10 +1,60 @@
+<?php  
 
-<?php 
- $fields = "id,caption,media_url,permalink,thumbnail_url,username" ;
- $token = "IGQVJWYmVCSFl3M0NDeGVnN0ZAiM1RvTTZAwT1k2Q1hxd2JtaEpYWWJSeUcySHpWN0ZAJcHdSLXNxY2VKRFg2d1JEZADFWVC0xZAVdveGpqRS1mdVI5RGNBenRoS1Y3WkVFZAXVlTkdnX0U1MFVGSFdYenA5NwZDZD";
- $limit = 6;
- $json_feed_url="https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
- $json_feed = file_get_contents($json_feed_url);
- $contents = json_decode($json_feed,true, 512, JSON_BIGINT_AS_STRING);
+class instagram_data {
+    // Properties
+    public $fields;
+    public $token;
+    public $limit;
+    public $json_feed_url;
+    public $json_feed;
+    public $contents;
+  
+    // Methods
+     function fields($fields) { // add order
+        $this->$fields = "id,caption,media_url,permalink,thumbnail_url,username";
+      }
+
+       function token($token) { // add order
+        $this->$token = "IGQVJWYmVCSFl3M0NDeGVnN0ZAiM1RvTTZAwT1k2Q1hxd2JtaEpYWWJSeUcySHpWN0ZAJcHdSLXNxY2VKRFg2d1JEZADFWVC0xZAVdveGpqRS1mdVI5RGNBenRoS1Y3WkVFZAXVlTkdnX0U1MFVGSFdYenA5NwZDZD";
+      }
+
+       function limit($limit) { // add order
+        $this->$limit = 6;
+      }
+
+       function json_feed_url($json_feed_url, $fields, $token, $limit) { // add order
+        $this->$json_feed_url = "https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
+ 
+      }
+
+       function json_feed($json_feed, $json_feed_url) { // add order
+        $this->$json_feed = file_get_contents($json_feed_url);
+      }
+
+       function contents($contents, $json_feed_url) { // add order
+       return  $this->$contents = file_get_contents($json_feed_url);
+
+      }
+
+    }
+ 
+ 
+
+    foreach($contents["data"] as $post){
+        $media_url = isset($post["media_url"]) ? $post["media_url"] :"";
+        $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
+        $username = isset($post["username"]) ? $post["username"] : "";
+        $caption = isset($post["caption"]) ? $post["caption"] : "";
+        
+        echo "<div class='insta_post_container'>
+        
+            <a href='{$permalink}' >
+            <img  src='{$media_url}' />  </a>
+            <strong>@{$username}</strong> {$caption}
+          
+           </div>";
+        }
+  
+   
+   
 ?>
-
--- index.php
+++ index.php
@@ -9,44 +9,9 @@
 </head>
 <body>
 
-<?php include('./assets/php/instagram.php') ?>
-
-<!-- heading of this app -->
 <div class='heading_insta_feed'>Instagram Feed</div>
-<!-- this is a parent class -->
-<div class='insta_maindiv_container'>
- 
-<?php 
- foreach($contents["data"] as $post){
-     $media_url = isset($post["media_url"]) ? $post["media_url"] :"";
-     $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
-     $username = isset($post["username"]) ? $post["username"] : "";
-     $caption = isset($post["caption"]) ? $post["caption"] : "";
-     
-     echo "<div class='insta_post_container'>
-         <a href='{$permalink}' >
-         <img  src='{$media_url}' />  </a>
-         <strong>@{$username}</strong> {$caption}
-       
-        </div>";
- }
-?>
-</div>
 
-
-
-
-<!-- this is not working code -->
-
-<!-- <div class='insta_maindiv_container'>
-    <div class='insta_post_container'>
-      
-            <img src="<? //php echo $media_url ?>"/> 
-            <p><?php // echo $username ?></p>
-            <p><?php // echo $caption ?></p>
-            <a href="<?php // echo $permalink ?>">insta</a>
-    </div>
-</div> -->
+<?php include('./assets/php/instagram.php') ?>
 
 
 </body>
