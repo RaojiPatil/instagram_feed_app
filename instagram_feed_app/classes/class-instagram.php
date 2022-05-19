ec6b09081b0123a800652dd6e8de3442149146ce
Raoji Patil <raojipatil22@gmail.com>
2022-05-19 09:00:49 +0530

    update

--- instagram_feed_app/classes/class-instagram.php
+++ instagram_feed_app/classes/class-instagram.php
@@ -21,29 +21,29 @@ if ( ! class_exists( 'Class_Instagram' ) ) :
 
 
 		public function get_instagram_data() {
-            $fields = "id,caption,media_url,permalink,thumbnail_url,username" ;
-            $token = "IGQVJWYmVCSFl3M0NDeGVnN0ZAiM1RvTTZAwT1k2Q1hxd2JtaEpYWWJSeUcySHpWN0ZAJcHdSLXNxY2VKRFg2d1JEZADFWVC0xZAVdveGpqRS1mdVI5RGNBenRoS1Y3WkVFZAXVlTkdnX0U1MFVGSFdYenA5NwZDZD";
-            $limit = 6;
-            $json_feed_url="https://graph.instagram.com/me/media?fields={$fields}&access_token={$token}&limit={$limit}";
-            $json_feed = file_get_contents($json_feed_url);
-            $contents = json_decode($json_feed,true, 512, JSON_BIGINT_AS_STRING);
-            return $contents;
+            $insta_fields = "id,caption,media_url,permalink,thumbnail_url,username" ;
+            $token_id = "IGQVJWYmVCSFl3M0NDeGVnN0ZAiM1RvTTZAwT1k2Q1hxd2JtaEpYWWJSeUcySHpWN0ZAJcHdSLXNxY2VKRFg2d1JEZADFWVC0xZAVdveGpqRS1mdVI5RGNBenRoS1Y3WkVFZAXVlTkdnX0U1MFVGSFdYenA5NwZDZD";
+            $post_limit = 6;
+            $feed_url="https://graph.instagram.com/me/media?fields={$insta_fields}&access_token={$token_id}&limit={$post_limit}";
+            $feed = file_get_contents($feed_url);
+            $result = json_decode($feed,true, 512, JSON_BIGINT_AS_STRING);
+            return $result;
         }       
         
 		public function feed_output() {
 
-            $contents = $this->get_instagram_data();
+            $result = $this->get_instagram_data();
 
             ?>
             <!-- heading of this app -->
             <div class = 'heading_insta_feed'>Instagram Feed</div>
             <!-- this is a parent class -->
 
-            <?php if( is_array( $contents ) ) { ?>
+            <?php if( is_array( $result ) ) { ?>
                 <div class = 'insta_maindiv_container'>
                     <?php
 
-                        foreach( $contents["data"] as $post ){
+                        foreach( $result["data"] as $post ){
                             $media_url = isset( $post[ "media_url" ] ) ? $post["media_url"] :"";
                             $permalink = isset($post["permalink"]) ? $post["permalink"] : "";
                             $username = isset($post["username"]) ? $post["username"] : "";
